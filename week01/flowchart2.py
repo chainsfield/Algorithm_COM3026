4fcdb9a6a2e7ff6bee105b745fa3a82b357c8210
Gyunam Park <parkguenam@gmail.com>
2022-06-06 13:17:26 +0900

    update

--- week01/flowchart2.py
+++ week01/flowchart2.py
@@ -4,10 +4,10 @@ low = 0
 high = len(data) - 1
 while True:
     mid = (low + high) // 2
-    if target == data[mid]:
-        print('찾으시는 숫자는 데이터셋 중 %d번째에 존재합니다.'%(mid + 1))
-        break
-    elif target < data[mid]:
+    if target < data[mid]:
         high = mid - 1
     elif target > data[mid]:
         low = mid + 1
+    elif target == data[mid]:
+        print('찾으시는 숫자는 데이터셋 중 %d번째에 존재합니다.' % (mid + 1))
+        break
